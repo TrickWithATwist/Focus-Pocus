2a2f314a9670453ef19119e5feae541079632333
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-01 18:14:10 -0400

    Thing so far
    WIP, will indicate when the final version is done

---
+++ focuspocus/background.js
@@ -0,0 +1,113 @@
+
+
+//on/off button stuff
+//0 = off 1 = on
+var onoffstatus;
+
+
+//asynchronously loading in data hopefully?
+//if button has already been pressed then the value that has been saved to the local storage will be new value
+//for onoffstatus else the program will go on as usual as if onoffstatus is equal to 0
+try
+{
+    chrome.storage.local.get(["status"]).then((value) =>
+    {
+        console.log("data get successful");
+        onoffstatus = value.status;
+    })
+}
+catch(err) 
+{
+    /*this only happens in the case where the user uses the chrome extension for the first time or turns it 
+    on after turning it off.*/
+    onoffstatus = 0;
+}
+
+
+
+//varible for the url data and stuff
+var link; 
+try
+{
+    //value of link will be defined if there has been a link that has been saved to local storage
+    chrome.storage.local.get(["keylink"]).then((value) =>
+    {
+        console.log("link data get successful")
+        link = value.keylink;
+    })
+}
+catch(err)
+{
+    //if there has been no link that has been saved to the local storage then there will be no asynchronous preloading until there is
+    null;
+}
+
+
+//lisenting for messages
+chrome.runtime.onMessage.addListener(data =>
+    {
+        const{event} = data
+        switch(event)
+        {
+            case "onoff":
+                //code to toggle in between on and off for the button and then save it 
+                onoffstatus = 1 - onoffstatus;
+                console.log(onoffstatus);
+                handlestatus();
+            break;
+            case "add":
+                //link is stored in link variable
+                link = chrome.tabs.query;
+                //link will be added to local storage data via function
+                addlink();
+            break;
+            case "debug":
+                //this case logs different values to console to see variable values
+                if (link !== undefined)
+                {
+                    console.log(link);
+                }
+            break;
+            case "default":
+                //empty
+            break;
+        }
+        
+
+    }
+    )
+
+
+//functions
+const handlestatus = () =>
+{
+    const data = {status: onoffstatus};
+    //put this here to make sure that the code is actually working idk
+    console.log("data has been saved"); 
+    chrome.storage.local.set(data);
+}
+
+//function to add link to storage so that when the extension is running it can remember what web page user wants to 
+//focus on
+const addlink = () =>
+{
+    //setting data for the url that is going to be focused on 
+    chrome.storage.local.set({keylink: link}).then(() =>
+    {
+        console.log("link saved");
+    })
+}
+
+//function to remove link from local storage
+const removelink = () =>
+{
+    //removing the value from the key or something idk 
+    try
+    {
+        chrome.storage.local.remove(["keylink"]);
+    }
+    catch(err)
+    {
+        console.log("idk there was an error or something ig you shouldn't remove a link that you didn't even add you dingus")
+    }
+}
---
+++ focuspocus/popup/popup.js
@@ -0,0 +1,21 @@
+//setting up the elements
+const onoffelement = document.getElementById("onoff")
+const addelement = document.getElementById("addbutton")
+const clearelement = document.getElementById("clearbutton")
+const instructionelement = document.getElementById("instructions")
+const debugelement = document.getElementById("debug")
+
+//on off button function and essential values 
+onoffelement.onclick = function()
+{
+    chrome.runtime.sendMessage({event: "onoff"});
+}
+addelement.onclick = function()
+{
+    chrome.runtime.sendMessage({event:"add"})
+}
+debugelement.onclick = function()
+{
+    chrome.runtime.sendMessage({event: "debug"});
+}
+
