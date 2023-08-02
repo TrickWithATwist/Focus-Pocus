eb879af87caca97be7f44c380ad80fb806402153
TrickWithATwist <78521495+TrickWithATwist@users.noreply.github.com>
2023-08-02 14:53:49 -0400

    idk what im doign

--- focuspocus/background.js
+++ focuspocus/background.js
@@ -114,7 +114,7 @@ chrome.tabs.onCreated.addListener(async () =>
     //check the url if it is not instructions or the saved url then redirect if onoffstatus = 1
     //establishing variable that represents current url 
     currenturl = await getCurrentTabURL();
-    if((currenturl !== link) ^ (currenturl !== instructionurl))
+    if((currenturl !== link) && (currenturl !== instructionurl))
     {
         if(onoffstatus == 1)
         {
